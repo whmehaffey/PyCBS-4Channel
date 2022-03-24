ba009f29275bb2a8161b8220c218a1d324a97b81
whmehaffey <w.hamish@gmail.com>
2022-03-24 12:08:02 -0700

    Update AudioRecorderFunctions.py
    Revert to fix some code deletec by accident, then restored some fixes and set plot time to 500ms.

--- AudioRecorderFunctions.py
+++ AudioRecorderFunctions.py
@@ -1,4 +1,3 @@
-
 from PyQt5 import QtGui, QtCore, QtWidgets
 
 import pyaudio 
@@ -37,11 +36,66 @@ def RescanInputs():
 
     GlobalVars.numdevices=inputdevices
     p.terminate()
+
+
+def TriggeredRecordAudio(ui):
+
+ import GlobalVars
+ global graph_win1
+ global graph_win2
+ global graph_win3
+ global graph_win4
+
+ from pydub import AudioSegment
+ import array
+ import pdb
+ 
+ 
+ RATE = int(ui.SampleRatecomboBox.currentText());# sampling frequency
+ MIN_DUR=GlobalVars.buffertime*2+0.1;#
+
+ SILENCE_LIMIT = GlobalVars.buffertime;
+ PREV_AUDIO = GlobalVars.buffertime;
+
+ p = pyaudio.PyAudio()
+
+ CHANNELS=GlobalVars.CHANNELS;
+ rel = int(RATE/(CHUNK))
+ last_val_high = [0] * CHANNELS
+ last_val_low = [0] * CHANNELS
+ last_data = [0] * CHANNELS
+ 
+
+ stream=p.open(format=FORMAT,input_device_index=GlobalVars.inputdeviceindex,channels=GlobalVars.CHANNELS,rate=RATE,
+               input=True,
+               frames_per_buffer=CHUNK)
     
- plot_win1 = deque(maxlen=1*rel)
- plot_win2 = deque(maxlen=1*rel)
- plot_win3 = deque(maxlen=1*rel)
- plot_win4 = deque(maxlen=1*rel)
+ ui.ListeningTextBox_1.setText('<span style="color:green">quiet</span>')
+ ui.ListeningTextBox_2.setText('<span style="color:green">quiet</span>')
+ ui.ListeningTextBox_3.setText('<span style="color:green">quiet</span>')
+ ui.ListeningTextBox_4.setText('<span style="color:green">quiet</span>')
+ 
+ audio2send1 = [] 
+ audio2send2 = []
+ audio2send3 = []
+ audio2send4 = []
+ 
+
+ prev_audio1 = deque(maxlen=PREV_AUDIO * rel) #prepend audio running buffer
+ prev_audio2 = deque(maxlen=PREV_AUDIO * rel) #prepend audio running buffer
+ prev_audio3 = deque(maxlen=PREV_AUDIO * rel) #prepend audio running buffer
+ prev_audio4 = deque(maxlen=PREV_AUDIO * rel) #prepend audio running buffer
+ 
+ perm_win1 = deque(maxlen=PREV_AUDIO*rel)
+ perm_win2 = deque(maxlen=PREV_AUDIO*rel)
+ perm_win3 = deque(maxlen=PREV_AUDIO*rel)
+ perm_win4 = deque(maxlen=PREV_AUDIO*rel)
+
+
+ plot_win1 = deque(maxlen=math.ceil(0.5*rel)) # ! 500ms of display;
+ plot_win2 = deque(maxlen=math.ceil(0.5*rel))
+ plot_win3 = deque(maxlen=math.ceil(0.5*rel))
+ plot_win4 = deque(maxlen=math.ceil(0.5*rel))
  
  started1 = False
  started2 = False
@@ -77,17 +131,21 @@ def RescanInputs():
   #sound, last_val_low=low_pass_filter_by_chunk(sound, 15000, last_val_low)
   #sound, last_val_high, last_data=high_pass_filter_by_chunk(sound, int(ui.HighPassspinBox.value()), last_val_high,last_data)
   
-  channels = sound.split_to_mono();  
+  channels = sound.split_to_mono();
+
+  
 
   if (ui.Ch1checkBox.isChecked()):
       
-      ch1=channels[0].raw_data;    
+      ch1=channels[0].raw_data;
+    #  pdb.set_trace();
       perm_win1.append(ch1)
       plot_win1.append(ch1)
       data = b''.join(list(plot_win1))
-      plotarray1 = array.array("h",data);    
+      plotarray1 = array.array("h",data);
+      #pdb.set_trace();
 
-      if(sum([x > (GlobalVars.threshold1) for x in plotarray1])>25 and len(audio2send1)<MAX_DUR*rel):  
+      if(sum([x > (GlobalVars.threshold1) for x in plotarray1])>100 and len(audio2send1)<MAX_DUR*rel):  
        if(not started1):
           ui.ListeningTextBox_1.setText('<span style="color:red">singing</span>')
           started1 = True
@@ -113,11 +171,11 @@ def RescanInputs():
 
       ch2=channels[1].raw_data;    
       plot_win2.append(ch2)
-      perm_win2.append(ch2)
+      perm_win1.append(ch2)
       data = b''.join(list(plot_win2))
       plotarray2 = array.array("h",data); 
       
-      if (sum([x > GlobalVars.threshold2 for x in plotarray2])>25 and len(audio2send2)<MAX_DUR*rel):
+      if (sum([x > GlobalVars.threshold2 for x in plotarray2])>100 and len(audio2send2)<MAX_DUR*rel):
        if(not started2):
           ui.ListeningTextBox_2.setText('<span style="color:red">singing</span>')
           started2 = True
@@ -139,14 +197,14 @@ def RescanInputs():
        prev_audio2.append(ch2)
 
   if (ui.Ch3checkBox.isChecked()):
+      ch3=channels[2].raw_data;
       
-      ch3=channels[2].raw_data;      
       plot_win3.append(ch3)       
       perm_win3.append(ch3)
       data = b''.join(list(plot_win3))
       plotarray3 = array.array("h",data);                 
       
-      if (sum([x > GlobalVars.threshold3 for x in plotarray3])>25 and len(audio2send3)<MAX_DUR*rel):
+      if (sum([x > GlobalVars.threshold3 for x in plotarray3])>100 and len(audio2send3)<MAX_DUR*rel):
        if(not started3):
           ui.ListeningTextBox_3.setText('<span style="color:red">singing</span>')
           started3 = True
@@ -179,7 +237,7 @@ def RescanInputs():
       data = b''.join(list(plot_win4))
       plotarray4 = array.array("h",data);       
       
-      if (sum([x > GlobalVars.threshold4 for x in plotarray4])>25 and len(audio2send4)<MAX_DUR*rel):
+      if (sum([x > GlobalVars.threshold4 for x in plotarray4])>100 and len(audio2send4)<MAX_DUR*rel):
        if(not started4):
           ui.ListeningTextBox_4.setText('<span style="color:red">singing</span>')
           started4 = True
@@ -240,9 +298,3 @@ def save_audio(data,rootdir,filename):
  wf.close()
  return filename + '.wav'
  
-
-##
-##if(__name__ == '__main__'):
-## audio_int()
-## record_song()
-
